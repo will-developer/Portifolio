1756a5ba6b0979cbae032c8a14bdc314a267fe2d
will-developer <william_limaa@hotmail.com>
2024-07-15 13:36:09 -0300

    sobre-mim & habilidades

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+/* ========== Redimencionar a Navegação ao Scrolar ========= */
+var navbar = document.querySelector('.navbar');
+// quando o scroll passar de 20vw, será adicionado a classe 'sticky' na navbar no html
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navbar.classList.add('sticky')
+    : navbar.classList.remove('sticky');
+};
